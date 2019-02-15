10453acc4c9b43579bb84cbcef08302a294e3383
bodrovis <bodrovis@protonmail.com>
2019-02-15 20:32:45 +0300

    4.2

--- demo.exs
+++ demo.exs
@@ -1,3 +1,13 @@
 defmodule Demo do
-  # functions...
+  def my_func(b) do
+    (b + 2) |> mult |> division(2)
+  end
+
+  def mult(arg) do
+    arg * 2
+  end
+
+  def division(arg, d), do: arg / d
 end
+
+Demo.my_func(3) |> IO.inspect
