129de209fc0b24f5f2d197e9ebb9eee77bc2c16d
bodrovis <bodrovis@protonmail.com>
2019-02-19 18:24:48 +0300

    4.6

--- demo.exs
+++ demo.exs
@@ -1,18 +1,10 @@
 defmodule Demo do
   def run(list) do
-    printer = fn
-      {name, number} -> # body 1
-        "#{name} is #{number}" |> IO.puts
-      str -> # body 2
-        str |> IO.inspect
-      _ -> # fallback
-        IO.inspect "fallback!"
-    end
-
-    #Enum.each list, fn(str) -> str |> IO.inspect end
-    Enum.each list, printer
+    # capture operator
+    Enum.each list, &IO.inspect/1
+    # &( IO.inspect(&1 * &1) )
+    # fn(str) -> str |> IO.inspect end
   end
 end
 
 Demo.run [1,2,3,4,5]
-Demo.run [{:a, 1}, {:b, 2}, {:c, 3}, {:d, 4}, {:e, 5}]
