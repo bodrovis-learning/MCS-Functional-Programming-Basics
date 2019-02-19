d284085ee146f59e690c0e7cd75a8004a221f8d8
bodrovis <bodrovis@protonmail.com>
2019-02-19 19:08:56 +0300

    5-1

--- demo.exs
+++ demo.exs
@@ -1,10 +1,15 @@
 defmodule Demo do
-  def run(list) do
-    # capture operator
-    Enum.each list, &IO.inspect/1
-    # &( IO.inspect(&1 * &1) )
-    # fn(str) -> str |> IO.inspect end
+  def run(arg) do
+    if arg > 3 do
+      arg = arg * 2
+      arg + 2
+    #else
+    #  arg = arg / 2
+    #  arg + 10
+    end
+
+    if arg > 3, do: arg + 2 #, else: arg + 10
   end
 end
 
-Demo.run [1,2,3,4,5]
+Demo.run(2) |> IO.inspect
