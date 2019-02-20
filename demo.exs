8090daf56a3477ec4c5a734e3b42f1ad98059d37
bodrovis <bodrovis@protonmail.com>
2019-02-20 22:06:29 +0300

    6-7

--- demo.exs
+++ demo.exs
@@ -1,11 +1,17 @@
 defmodule Demo do
-  def run do
-    1..10000000 |>
-    Stream.map( &(&1 * 2) ) |>
+  def run(filename) do
+    printer = fn({contents, line_num}) ->
+      :timer.sleep 500
+      IO.puts "#{line_num} \t #{contents}"
+    end
+
+    #File.open!(filename) |>
+    #IO.stream(:line) |>
+    File.stream!(filename) |>
+    Stream.map(&( String.replace(&1, "\n", "") )) |>
     Stream.with_index |>
-    #Enum.take(3)
-    Enum.to_list
+    Enum.each(printer)
   end
 end
 
-Demo.run |> IO.inspect
+Demo.run "demo.exs"
