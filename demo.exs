2ff00dfbd897a881b4215d52b5d149bff662b1ae
bodrovis <bodrovis@protonmail.com>
2019-02-20 21:20:06 +0300

    6-6

--- demo.exs
+++ demo.exs
@@ -1,18 +1,11 @@
 defmodule Demo do
-  def fact(n), do: do_fact 1, n
-
-  defp do_fact(total, n) when n < 1, do: total
-  defp do_fact(total, n) do
-    total * n |> do_fact(n - 1)
-  end
-
-  def mult(list), do: do_mult(1, list)
-
-  defp do_mult(result, []), do: result
-  defp do_mult(result, [head | tail]) do
-    result * head |> do_mult(tail)
+  def run do
+    1..10000000 |>
+    Stream.map( &(&1 * 2) ) |>
+    Stream.with_index |>
+    #Enum.take(3)
+    Enum.to_list
   end
 end
 
-#Demo.fact(100) |> IO.inspect
-Demo.mult([1,2,3,4]) |> IO.inspect
+Demo.run |> IO.inspect
