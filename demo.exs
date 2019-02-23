3bc3e6ff68a6a5b07bac077ad48ddf9227f499d2
bodrovis <bodrovis@protonmail.com>
2019-02-23 20:32:43 +0300

    8-4

--- demo.exs
+++ demo.exs
@@ -1,37 +1,7 @@
-defmodule DemoMacro do
-  defmacro my_macro(number) do
-    quote do
-      unquote(number) * 10
-    end
-  end
-
-  defmacro m_time(time) do
-    quote bind_quoted: [time: time] do
-      time |> IO.inspect
-      :timer.sleep 2000
-      time |> IO.inspect
-    end
-  end
-
-  defmacro m_palindrome?(str, true_clause) do
-    quote bind_quoted: [str: str, true_clause: true_clause] do
-      if str == String.reverse(str), do: true_clause
-    end
-  end
-
-  def palindrome?(str, true_clause) do
-    if str == String.reverse(str), do: true_clause
+defimpl String.Chars, for: Map do
+  def to_string(term) do
+    term |> Map.keys |> Enum.join(", ")
   end
 end
 
-defmodule Demo do
-  require DemoMacro
-
-  def run do
-    DemoMacro.m_time :os.system_time
-    #DemoMacro.m_palindrome?("kabak", IO.puts("YES!")) |> IO.inspect # :ok
-    #DemoMacro.my_macro(2) |> IO.inspect
-  end
-end
-
-Demo.run
+%{ test: 1, another_key: 2 } |> IO.puts
