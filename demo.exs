0d116aa8bc9970a6fc067ab1a6e4560ef304512a
bodrovis <bodrovis@protonmail.com>
2019-02-23 19:35:40 +0300

    8-2

--- demo.exs
+++ demo.exs
@@ -1,8 +1,18 @@
 defmodule DemoMacro do
   defmacro my_macro(number) do
     quote do
-      number * 10
+      unquote(number) * 10
     end
+  end
+
+  defmacro m_palindrome?(str, true_clause) do
+    quote do
+      if unquote(str) == String.reverse(unquote(str)), do: unquote(true_clause)
+    end
+  end
+
+  def palindrome?(str, true_clause) do
+    if str == String.reverse(str), do: true_clause
   end
 end
 
@@ -10,7 +20,8 @@ defmodule Demo do
   require DemoMacro
 
   def run do
-    DemoMacro.my_macro(2) |> IO.inspect
+    DemoMacro.m_palindrome?("kabak", IO.puts("YES!")) |> IO.inspect # :ok
+    #DemoMacro.my_macro(2) |> IO.inspect
   end
 end
 
